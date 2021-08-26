eb5683ffa5ec8e52c59cd9ed573d2c18c9c4b6f3
mustafa-mahmud <mithuweb000@gmail.com>
2021-08-26 10:09:30 +0600

    randRage() add

--- app.js
+++ app.js
@@ -3,5 +3,5 @@
 const nums = randAll(10, 20, 30, 40, 50, 100);
 const arr = randAll('ball', 'a', 'b', 'c', 'd');
 
-console.log(nums);
-console.log(arr);
+const data = randRange(3, 7);
+console.log(data);
--- my-library.js
+++ my-library.js
@@ -19,3 +19,32 @@ function randAll(...arr) {
 
   return send;
 }
+
+function randRange(data1, data2) {
+  let store = [];
+  const num1 = Math.abs(data1);
+  const num2 = Math.abs(data2);
+  let min = num1 < num2 ? num1 : num2;
+  const max = num1 > num2 ? num1 : num2;
+  min = min + 1;
+
+  if (arguments.length > 2 || arguments.length < 2)
+    throw Error('You must put 2 numbers');
+
+  if (!Number.isInteger(num1) || !Number.isInteger(num2))
+    throw Error('Only number supports');
+
+  function doRandRange() {
+    const rand = Math.floor(Math.random() * (max - min) + min);
+
+    if (!store.includes(rand)) store.push(rand);
+
+    if (store.length < max - min) {
+      doRandRange();
+    }
+  }
+
+  doRandRange();
+
+  return store;
+}
